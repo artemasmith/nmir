96a6446862df772a93e9cde402877dc25cf39c4b
tea <teacplusplus@gmail.com>
2014-11-21 06:39:54 +0300

    fixes

--- app/indices/advertisement_index.rb
+++ app/indices/advertisement_index.rb
@@ -9,7 +9,7 @@ ThinkingSphinx::Index.define :advertisement,
   indexes landmark, sortable: true
   indexes locations.title, :as => :location_titles, sortable: true
   has locations.id, :as => :location_ids
-  %w(created_at updated_at price_from price_to adv_type property_type category offer_type mortgage not_for_agents status_type floor_from floor_cnt_from space_from outdoors_space_from floor_to floor_cnt_to space_to outdoors_space_to).each do |m|
+  %w(created_at updated_at price_from price_to adv_type property_type category offer_type mortgage not_for_agents status_type floor_from floor_cnt_from space_from outdoors_space_from floor_to floor_cnt_to space_to outdoors_space_to room_from room_to).each do |m|
     has m, as: m
   end
 end
