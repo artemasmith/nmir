dc579836c9a9c26bed5793f969e2b1bf7084a1c0
tea <teacplusplus@gmail.com>
2015-02-18 00:21:47 +0300

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,12 +82,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # config.action_mailer.sendmail_settings = {
-  #   location: '/usr/sbin/sendmail',
-  #   arguments: '-i -t'
-  # }
+  config.action_mailer.sendmail_settings = {
+    location: '/usr/sbin/sendmail',
+    arguments: '-i'
+  }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_options = {from: 'multilisting@multilisting.su'}
 end
