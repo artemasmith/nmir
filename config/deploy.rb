7d09b38640966c242f5382747a46025c6dbf86a6
tea <teacplusplus@gmail.com>
2015-04-09 07:13:20 +0300

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for Capistrano 3.1
-lock '3.2.1'
+lock '3.4.0'
 set :application, 'multilisting'
 set :repo_url, 'git@github.com:teacplusplus/nmir.git'
 set :rails_env, "production"
